f460f6db1538fa2c0fca4d9969d2121c1caae29c
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-03-26 10:44:48 -0400

    Added argparser functionality to accept inputs from the command line

---
+++ doc_parser.py
@@ -0,0 +1,27 @@
+# Import libaries.
+import argparse
+import os
+import sys
+
+# Infrastructure to set up command line argument parser.
+parser = argparse.ArgumentParser(description='Enter the directory containing the .txt files you wish to analyze'
+                                             'as well as the number of interesting words you would like to observe')
+parser.add_argument("-d", "--directory", type=str, help='Path to the directory containing the .txt files')
+parser.add_argument("-n", "--number_words", type=int, help='Number of interesting words you would like parsed'
+                                                           'ranked by popularity, must be less than 100)')
+
+args = parser.parse_args()
+directory = args.directory
+N = args.number_words
+
+if not os.path.isdir(directory):
+    print('The path specified does not exist')
+    sys.exit()
+
+
+def main():
+    print(f"The directory specified is {directory} with an N value of {N}.")
+
+
+if __name__ == "__main__":
+    main()
