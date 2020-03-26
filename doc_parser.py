933785ec09546d8a59ddcfe5b2dfc98dcd870faf
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-03-26 10:47:57 -0400

    Added comments

--- doc_parser.py
+++ doc_parser.py
@@ -5,8 +5,10 @@ import sys
 
 # Infrastructure to set up command line argument parser.
 parser = argparse.ArgumentParser(description='Enter the directory containing the .txt files you wish to analyze'
-                                             'as well as the number of interesting words you would like to observe')
-parser.add_argument("-d", "--directory", type=str, help='Path to the directory containing the .txt files')
+                                             'as well as the number of interesting words you would like to observe'
+                                             'DIRECTORY MUST NOT CONTAIN ANY SPACES IN FOLDER NAMES')
+parser.add_argument("-d", "--directory", type=str, help='Path to the directory containing the .txt files'
+                                                        'DIRECTORY MUST NOT CONTAIN ANY SPACES IN FOLDER NAMES')
 parser.add_argument("-n", "--number_words", type=int, help='Number of interesting words you would like parsed'
                                                            'ranked by popularity, must be less than 100)')
 
@@ -14,6 +16,7 @@ args = parser.parse_args()
 directory = args.directory
 N = args.number_words
 
+# Input validation for directory.
 if not os.path.isdir(directory):
     print('The path specified does not exist')
     sys.exit()
